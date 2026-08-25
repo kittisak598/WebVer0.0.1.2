b64b41400774a8efbf1fd70deebc3a81c231bec0
unknown <kzkittisak2@gmail.com>
2026-08-25 16:34:58 +0700

    up db.js api.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "loveanimal",
+    port: 3306
 });
 
 db.connect((err) => {
     if (err) {
+        console.log("เชื่อมต่อฐานข้อมูลไม่สำเร็จ");
         console.log(err);
     } else {
         console.log("Database Connected");
